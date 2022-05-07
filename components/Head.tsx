59ccf09ec2d3583dedf4a7053ebfec57249e57be
uzimaru0000 <shuji365630@gmail.com>
2022-05-07 15:21:26 +0900

    :horse: ベースのOGPを設定

--- components/Head.tsx
+++ components/Head.tsx
@@ -6,6 +6,18 @@ export const Head: React.VFC<React.PropsWithChildren<{}>> = ({ children }) => {
     <NextHead>
       <title>{"uzimaru's blog"}</title>
       <meta name="description" content="うじまるのブログ" />
+      <meta property="og:url" content="https://blog.uzimaru.com" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="うじまるのブログ" />
+      <meta
+        property="og:description"
+        content="うじまるのブログです。技術的なことや日常的なことを書いていきます。"
+      />
+      <meta property="og:site_name" content="uzimaru's blog" />
+      <meta property="og:image" content="https://blog.uzimaru.com/ogp.png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta name="twitter:card" content="summary_large_image" />
       <link rel="icon" href={fabicon} />
       {children}
     </NextHead>
