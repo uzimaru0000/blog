34a5fb9f3615f22fe315fedb3a9587821f47d4fe
uzimaru0000 <shuji365630@gmail.com>
2022-06-05 22:49:18 +0900

    :bug: add types

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,16 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+interface window {
+  gtag(type: 'config', googleAnalyticsId: string, { page_path: string });
+  gtag(
+    type: 'event',
+    eventAction: string,
+    fieldObject: {
+      event_label: string;
+      event_category: string;
+      value?: string;
+    }
+  );
+}
